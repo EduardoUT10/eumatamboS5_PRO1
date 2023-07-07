9ac8a15557003796ef743da6906df9cc6a9aa1a0
GALOSANTY <galosanty123@gmail.com>
2023-07-07 18:26:14 -0500

    clases heredadas

---
+++ eumatamboS5.Android/MensajeAndroid.cs
@@ -0,0 +1,30 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using eumatamboS5.Droid;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+// para emsamblar  el archivo
+[assembly: Xamarin.Forms.Dependency(typeof(MensajeAndroid))]
+namespace eumatamboS5.Droid
+{
+    internal class MensajeAndroid : Mensaje
+    {
+        // para  heredar  la clase previamente creada
+        public void longAlert(string mensaje)
+        {
+            Toast.MakeText(Application.Context, mensaje, ToastLength.Long).Show();
+
+        }
+
+        public void shortAlert(string mensaje)
+        {
+            Toast.MakeText(Application.Context, mensaje, ToastLength.Short).Show();
+        }
+    }
+}
---
+++ eumatamboS5/Mensaje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eumatamboS5
+{
+    public interface Mensaje
+    {
+
+        //metodo a implementar  notificacion toast
+        void shortAlert(string mensaje);
+        void longAlert(string mensaje); 
+    }
+}
